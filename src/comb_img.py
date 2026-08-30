a1b719d7fb24e720b48a26c8a5be708bfa06ec8e
Eshani Fernando <fernandoeshani@gmail.com>
2026-08-30 19:59:24 +0000

    images

--- src/comb_img.py
+++ src/comb_img.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from matplotlib.gridspec import GridSpec
 
 img_top = mpimg.imread('visualizations_GDELT/combined_difficulty.png')
 img_bottom = mpimg.imread('visualizations_ICEWS14/combined_difficulty.png')
 
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(7.5, 7.0))
+fig = plt.figure(figsize=(7.5, 7.0))
+gs = GridSpec(2, 1, figure=fig, hspace=0, wspace=0,
+              left=0, right=1, top=1, bottom=0)  # zero all margins
 
-ax1.imshow(img_top)
+ax1 = fig.add_subplot(gs[0])
+ax2 = fig.add_subplot(gs[1])
+
+ax1.imshow(img_top, aspect='auto')
 ax1.axis('off')
 
-ax2.imshow(img_bottom)
+ax2.imshow(img_bottom, aspect='auto')
 ax2.axis('off')
 
-plt.subplots_adjust(hspace=0, wspace=0)  # no gap between the two
-plt.savefig('combined_two_datasets.tiff', dpi=600, bbox_inches='tight', pad_inches=0)
+plt.savefig('combined_two_datasets.tiff', dpi=600)
